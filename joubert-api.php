e00ebb793f81d549e73a8944b5c436d6928f745e
Jeremy <jspaeth@prevor.com>
2024-06-17 17:51:13 +0200

    Initial commit

---
+++ joubert-api.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Joubert API
+ * Plugin URI:
+ * Description:       Plugin to get data from Joubert API and display it in a chart.
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      8.0.2
+ * Author:            Jeremy SPAETH
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+
+
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
